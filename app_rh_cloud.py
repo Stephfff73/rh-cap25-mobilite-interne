d9d5b840a9433fe3813112953d3a86791e33da26
Stephfff73 <stephanie.ammi@inli.fr>
2026-02-07 11:48:37 +0100

    Update app_rh_cloud.py

--- app_rh_cloud.py
+++ app_rh_cloud.py
@@ -1,4 +1,4 @@
-pip install xlsxwriter
+import xlsxwriter
 import streamlit as st
 import pandas as pd
 from datetime import datetime, date
@@ -2229,6 +2229,7 @@ st.markdown("""
     <p>CAP25 - Pilotage de la Mobilité Interne | Synchronisé avec Google Sheets</p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
